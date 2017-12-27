877f587c3762fafbb3f7a0a559b8715066897c20
moannuo <demenet.alain@hotmail.fr>
2017-12-27 17:42:05 +0100

    Working on types, need to handle struct t_sym first

--- nm/get_type.c
+++ nm/get_type.c
@@ -6,17 +6,22 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/21 15:43:59 by ademenet          #+#    #+#             */
-/*   Updated: 2017/12/21 18:02:13 by ademenet         ###   ########.fr       */
+/*   Updated: 2017/12/27 16:13:58 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/ft_nm.h"
 
-static char		get_sect_type(uint8_t n_sect)
+static char		get_sect_type(uint8_t n_sect, t_sym *symbol)
 {
-	char		type;
-	
-	return (type);
+	if (n_sect == symbol->text)
+		return ('T');
+	else if (n_sect == symbol->data)
+		return ('D');
+	else if (n_sect == symbol->bss)
+		return ('B');
+	else
+		return ('S');
 }
 
 char			get_type(uint8_t n_type, uint8_t n_sect, int n_value, 
@@ -26,7 +31,9 @@ char			get_type(uint8_t n_type, uint8_t n_sect, int n_value,
 	uint8_t		masked;
 
 	masked = n_type & N_TYPE;
-	if (masked == N_UNDF)
+	if (n_type & N_STAB)
+		type = '-';
+	else if (masked == N_UNDF)
 		type = 'U';
 	else if (masked == N_ABS)
 		type = 'A';
