aeb62bd2a38f094f6d84c86b8460db590c19fa9b
moannuo <demenet.alain@hotmail.fr>
2018-01-03 17:46:08 +0100

    Norm done

--- nm/ft_nm.c
+++ nm/ft_nm.c
@@ -6,26 +6,18 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 15:49:57 by ademenet          #+#    #+#             */
-/*   Updated: 2017/12/29 15:14:19 by ademenet         ###   ########.fr       */
+/*   Updated: 2018/01/03 17:21:48 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/ft_nm.h"
 
-// TODO :
-// 1 - lire quel type de fichier > appel la bonne fonction
-// 2 - iterer sur les symbols et recuperer les infos
-
 /*
-** 
+**
 */
 
 static void		nm(char *ptr)
 {
-	// Nous regardons les 4 premiers octets du fichier afin de déterminer
-	// sur quel type d'architecture nous nous trouvons : 64 bits, fat
-	// binaries, etc.
-	// `hexdump -C -n 4 a.out`
 	int			magic_number;
 
 	magic_number = *(int *)ptr;
@@ -47,16 +39,17 @@ static int		iterate_over_files(void)
 	int			fd;
 	char		*ptr;
 	struct stat	buf;
-	
+
 	if ((fd = open(g_filename, O_RDONLY)) < 0)
 		return (error_display("No such file or directory."));
 	if (fstat(fd, &buf) < 0)
-		return(error_display("An error occured with fstat."));
-	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+		return (error_display("An error occured with fstat."));
+	if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0))
+		== MAP_FAILED)
 		return (error_display("An error occured with mmap."));
 	nm(ptr);
 	if (munmap(ptr, buf.st_size) < 0)
-		return(error_display("An error occured with munmap."));
+		return (error_display("An error occured with munmap."));
 	return (EXIT_SUCCESS);
 }
 
@@ -66,7 +59,7 @@ int				main(int ac, char **av)
 
 	if (ac == 1)
 	{
-		g_filename = "a.out";		
+		g_filename = "a.out";
 		iterate_over_files();
 	}
 	else
