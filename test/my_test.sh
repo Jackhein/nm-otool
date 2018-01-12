cf15631a7d5e7c1cacfd14255e9f01785f253993
moannuo <demenet.alain@hotmail.fr>
2018-01-12 11:53:40 +0100

    Small test using head and tail on binary file

---
+++ test/my_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+FILE=a.out
+
+FILESIZE=$(wc -c $FILE | awk '{print $1}')
+
+echo $FILESIZE
+
+for i in `seq 1 $FILESIZE`;
+do
+    echo "$i / $FILESIZE"
+    head -c$i $FILE > test_$i
+    # tail -c$i $FILE > test_$i
+    # tail -c+$i $FILE > test_$i
+    ./ft_nm test_$i
+    rm test_$i
+done
